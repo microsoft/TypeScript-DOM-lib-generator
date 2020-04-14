41f5e0973fbed8ebe1eae3adbe8538bd88ac4cff
Keith Cirkel <keithamus@users.noreply.github.com>
2020-04-14 12:36:25 +0100

    fix(dom): DOMRECTList index can return undefined

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -3992,7 +3992,7 @@ declare var SVGRect: typeof DOMRect;
 interface DOMRectList {
     readonly length: number;
     item(index: number): DOMRect | null;
-    [index: number]: DOMRect;
+    [index: number]: DOMRect | undefined;
 }
 
 declare var DOMRectList: {
