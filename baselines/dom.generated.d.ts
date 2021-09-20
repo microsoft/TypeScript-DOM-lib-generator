dc36038d361bc3993d0b67f22ecc7183ffe3faa5
ExE Boss <ExE-Boss@code.ExE-Boss.tech>
2021-09-20 08:30:00 +0200

    fix(WebAssembly): Correct `NativeError` subclass types

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -16552,7 +16552,8 @@ declare namespace WebAssembly {
 
     var CompileError: {
         prototype: CompileError;
-        new(): CompileError;
+        new(message?: string): CompileError;
+        (message?: string): CompileError;
     };
 
     interface Global {
@@ -16579,7 +16580,8 @@ declare namespace WebAssembly {
 
     var LinkError: {
         prototype: LinkError;
-        new(): LinkError;
+        new(message?: string): LinkError;
+        (message?: string): LinkError;
     };
 
     interface Memory {
@@ -16608,7 +16610,8 @@ declare namespace WebAssembly {
 
     var RuntimeError: {
         prototype: RuntimeError;
-        new(): RuntimeError;
+        new(message?: string): RuntimeError;
+        (message?: string): RuntimeError;
     };
 
     interface Table {
