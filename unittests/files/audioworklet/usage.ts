a39f4fe744a5c8d3eea7b6548e80c5d9b69a0328
Sebastian Krüger <2pi_r2@gmx.de>
2022-03-23 14:47:28 +0100

    fix test

--- unittests/files/audioworklet/usage.ts
+++ unittests/files/audioworklet/usage.ts
@@ -1,4 +1,4 @@
-self.registerProcessor(
+registerProcessor(
   "test",
   class extends AudioWorkletProcessor {
     process(input: Float32Array[][], output: Float32Array[][], params: { [key: string]: Float32Array }) {
