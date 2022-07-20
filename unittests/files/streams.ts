43cd68158d8fa6eb2c132d6aef4305aae35f5001
Mattias Buelens <mattias@buelens.com>
2022-07-21 00:41:28 +0200

    Add unit tests for constructing a byte stream

--- unittests/files/streams.ts
+++ unittests/files/streams.ts
@@ -1,11 +1,29 @@
-const readable = new ReadableStream({
+const readableByteStream = new ReadableStream({
   type: "bytes",
+  start(controller) {
+    controller.byobRequest;
+  },
+  pull(controller) {
+    if (controller.byobRequest) {
+      controller.byobRequest.view;
+      controller.byobRequest.respond(1);
+      controller.byobRequest.respondWithNewView(new Uint32Array(1));
+    } else {
+      controller.enqueue(new Uint8Array(1));
+    }
+    controller.close();
+  },
 });
 
-const defaultReader = readable.getReader();
-defaultReader.read();
+const defaultReader = readableByteStream.getReader();
+defaultReader.read().then((result) => {
+  if (!result.done) {
+    result.value.buffer;
+    result.value[0];
+  }
+});
 
-const byobReader = readable.getReader({ mode: "byob" });
+const byobReader = readableByteStream.getReader({ mode: "byob" });
 byobReader.read(new Uint8Array(1));
 byobReader.read(new DataView(new ArrayBuffer(1))).then((result) => {
   if (!result.done) {
