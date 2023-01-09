ef9854e8bc9998966fbdb59b9f43048b7448cc1c
Yann Brelière <yann@gandi.net>
2023-01-09 20:03:08 +0100

    Add more precise typing for autoComplete HTML attribute
    Replaces: microsoft/TypeScript#52169
    
    This will allow to autocomplete… the `autoComplete` attribute in vscode for example.
    
    ![autoComplete autocomplete in vscode](https://user-images.githubusercontent.com/58247/211378145-c01c665b-9f4b-4918-83cc-16532f555935.png)
    
    The list is quite long and it's not easy remembering the spelling of each one of them (first_name or given-name? password or current-password?), and it would improve the UX of sites if they were more appropriately used (for example, some people may think that the input's `type` and/or `name` attributes may be enough for fields like email or password, but knowing to use `autoComplete="new-password"` or `one-time-code` can be really useful).

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -10260,7 +10260,7 @@ interface HTMLFormElement extends HTMLElement {
      *
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLFormElement/autocomplete)
      */
-    autocomplete: string;
+    autocomplete: "off" | "on" | "name" | "honorific-prefix" | "given-name" | "family-name" | "honorific-suffix" | "nickname" | "email" | "username" | "new-password" | "current-password" | "one-time-code" | "organization-title" | "organization" | "street-address" | "address-line1" | "address-line2" | "address-line3" | "address-level4" | "address-level3" | "address-level2" | "address-level1" | "country" | "country-name" | "postal-code" | "cc-name" | "cc-given-name" | "cc-additional-name" | "cc-family-name" | "cc-number" | "cc-exp" | "cc-exp-month" | "cc-exp-year" | "cc-csc" | "cc-type" | "transaction-currency" | "transaction-amount" | "language" | "bday" | "bday-day" | "bday-month" | "bday-year" | "sex" | "tel" | "tel-country-code" | "tel-national" | "tel-area-code" | "tel-local" | "tel-extension" | "impp" | "url" | "photo" | (string & {});
     /**
      * Retrieves a collection, in source order, of all controls in a given form.
      *
