741495591d0eed50e3e36b8728d27cf54caf04d2
Gautier <gautier@escape.tech>
2023-06-29 22:28:13 +0200

    test: added unit tests

--- unittests/index.js
+++ unittests/index.js
@@ -3,16 +3,24 @@ import { readdir } from "fs/promises";
 
 for (const filename of await readdir(new URL("files/", import.meta.url))) {
   if (filename.endsWith(".ts")) {
-    execSync(`npx tsc generated/dom.generated.d.ts unittests/files/${filename} --target es2020 --lib es2020 --types --noEmit`, {
-      stdio: "inherit"
-    });
+    execSync(
+      `npx tsc generated/dom.generated.d.ts unittests/files/${filename} --strict --strictNullChecks --target es2020 --lib es2020 --types --noEmit`,
+      {
+        stdio: "inherit",
+      }
+    );
   }
 }
 
-for (const filename of await readdir(new URL("files/audioworklet", import.meta.url))) {
+for (const filename of await readdir(
+  new URL("files/audioworklet", import.meta.url)
+)) {
   if (filename.endsWith(".ts")) {
-    execSync(`npx tsc generated/audioworklet.generated.d.ts unittests/files/audioworklet/${filename} --target es2020 --lib es2020 --types --noEmit`, {
-      stdio: "inherit"
-    });
+    execSync(
+      `npx tsc generated/audioworklet.generated.d.ts unittests/files/audioworklet/${filename} --strict --strictNullChecks --target es2020 --lib es2020 --types --noEmit`,
+      {
+        stdio: "inherit",
+      }
+    );
   }
 }
