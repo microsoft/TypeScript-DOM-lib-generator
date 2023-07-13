de55f5543ec1c358a96f70c579110a859310f091
steveluscher <me+github@steveluscher.com>
2023-07-13 16:23:26 +0000

    Unambiguously assert the return type of `exportKey` when `format` is not known

--- unittests/files/keyusage.ts
+++ unittests/files/keyusage.ts
@@ -1,24 +1,44 @@
-function assertType<T>(_x: T) {}
-
 const mockKey = {} as CryptoKey;
-assertType<Promise<ArrayBuffer | JsonWebKey>>(crypto.subtle.exportKey('' as KeyFormat, mockKey));
-assertType<Promise<JsonWebKey>>(crypto.subtle.exportKey('jwk', mockKey));
-assertType<Promise<ArrayBuffer>>(crypto.subtle.exportKey('pkcs8', mockKey));
-assertType<Promise<ArrayBuffer>>(crypto.subtle.exportKey('raw', mockKey));
-assertType<Promise<ArrayBuffer>>(crypto.subtle.exportKey('spki', mockKey));
 
-const usageInline = crypto.subtle.generateKey({
-  name: "AES-GCM",
-  length: 256,
-},  true, ['encrypt', 'decrypt'])
+assertType<Promise<JsonWebKey>>(crypto.subtle.exportKey("jwk", mockKey));
+assertType<Promise<ArrayBuffer>>(crypto.subtle.exportKey("pkcs8", mockKey));
+assertType<Promise<ArrayBuffer>>(crypto.subtle.exportKey("raw", mockKey));
+assertType<Promise<ArrayBuffer>>(crypto.subtle.exportKey("spki", mockKey));
 
-const usageConst = crypto.subtle.generateKey( {
-  name: "AES-GCM",
-  length: 256,
-},  true, ['encrypt', 'decrypt'] as const)
+assertType<Promise<ArrayBuffer | JsonWebKey>>(
+  crypto.subtle
+    .exportKey("" as KeyFormat, mockKey)
+    .then((ambiguousKey) =>
+      ambiguousKey instanceof ArrayBuffer
+        ? (ambiguousKey satisfies ArrayBuffer)
+        : (ambiguousKey satisfies JsonWebKey)
+    )
+);
 
-const keyUsage: ReadonlyArray<KeyUsage> = ['encrypt', 'decrypt']
-const usageAsReadonly = crypto.subtle.generateKey( {
-  name: "AES-GCM",
-  length: 256,
-},  true, keyUsage)
+const usageInline = crypto.subtle.generateKey(
+  {
+    name: "AES-GCM",
+    length: 256,
+  },
+  true,
+  ["encrypt", "decrypt"]
+);
+
+const usageConst = crypto.subtle.generateKey(
+  {
+    name: "AES-GCM",
+    length: 256,
+  },
+  true,
+  ["encrypt", "decrypt"] as const
+);
+
+const keyUsage: ReadonlyArray<KeyUsage> = ["encrypt", "decrypt"];
+const usageAsReadonly = crypto.subtle.generateKey(
+  {
+    name: "AES-GCM",
+    length: 256,
+  },
+  true,
+  keyUsage
+);
