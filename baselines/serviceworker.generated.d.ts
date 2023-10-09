a918c73dce3569c92706d8c2a9104214e391ddaf
Ezra Adeyinka <adeyinkaezra123@gmail.com>
2023-10-09 08:17:52 +0100

    docs: update baselines to reflect typo changes

--- baselines/serviceworker.generated.d.ts
+++ baselines/serviceworker.generated.d.ts
@@ -542,6 +542,7 @@ interface RequestInit {
     referrer?: string;
     /** A referrer policy to set request's referrerPolicy. */
     referrerPolicy?: ReferrerPolicy;
+    sharedStorageWritable?: boolean;
     /** An AbortSignal to set request's signal. */
     signal?: AbortSignal | null;
     /** Can only be null. Used to disassociate request from any Window. */
@@ -644,6 +645,7 @@ interface TextEncoderEncodeIntoResult {
 }
 
 interface Transformer<I = any, O = any> {
+    cancel?: TransformerCancelCallback;
     flush?: TransformerFlushCallback<O>;
     readableType?: undefined;
     start?: TransformerStartCallback<O>;
@@ -4426,7 +4428,7 @@ declare var PushMessageData: {
 };
 
 /**
- * This Push API interface provides a subcription's URL endpoint and allows unsubscription from a push service.
+ * This Push API interface provides a subscription'sURL endpoint and allows unsubscription from a push service.
  * Available only in secure contexts.
  *
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PushSubscription)
@@ -7778,7 +7780,7 @@ interface WebSocket extends EventTarget {
      */
     readonly url: string;
     /**
-     * Closes the WebSocket connection, optionally using code as the the WebSocket connection close code and reason as the the WebSocket connection close reason.
+     * Closes the WebSocket connection, optionally using code as the WebSocket connection close code and reason as the WebSocket connection close reason.
      *
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/WebSocket/close)
      */
@@ -8361,6 +8363,10 @@ interface QueuingStrategySize<T = any> {
 
 interface ReportingObserverCallback {
     (reports: Report[], observer: ReportingObserver): void;
+}
+
+interface TransformerCancelCallback {
+    (reason: any): void | PromiseLike<void>;
 }
 
 interface TransformerFlushCallback<O> {
