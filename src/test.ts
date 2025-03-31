c899c09a1c722526b0829817710880cba7ef45bc
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2025-03-31 14:36:08 +0200

    -

--- src/test.ts
+++ src/test.ts
@@ -81,7 +81,6 @@ const compileGeneratedFiles = (lib: string[] | string, ...files: string[]) => {
     );
   } catch (e) {
     console.error(`Test failed: could not compile '${files.join(", ")}'.`);
-    console.error(e?.stdout ? e.stdout.toString() : e);
     return false;
   }
   return true;
