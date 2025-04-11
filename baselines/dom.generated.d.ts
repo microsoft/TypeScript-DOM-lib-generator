104ee30f4266b1675a601ad6d225eb695159649f
Kagami Sascha Rosylight <saschanaz@outlook.com>
2025-04-11 12:13:37 +0200

    Upgrade to BCD 6

--- baselines/dom.generated.d.ts
+++ baselines/dom.generated.d.ts
@@ -2598,6 +2598,7 @@ interface ARIAMixin {
     ariaValueNow: string | null;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Element/ariaValueText) */
     ariaValueText: string | null;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Element/role) */
     role: string | null;
 }
 
@@ -6797,6 +6798,7 @@ interface DOMMatrix extends DOMMatrixReadOnly {
     rotateSelf(rotX?: number, rotY?: number, rotZ?: number): DOMMatrix;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrix/scale3dSelf) */
     scale3dSelf(scale?: number, originX?: number, originY?: number, originZ?: number): DOMMatrix;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrix/scaleSelf) */
     scaleSelf(scaleX?: number, scaleY?: number, scaleZ?: number, originX?: number, originY?: number, originZ?: number): DOMMatrix;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMMatrix/setMatrixValue) */
     setMatrixValue(transformList: string): DOMMatrix;
@@ -11134,7 +11136,9 @@ interface HTMLFormElement extends HTMLElement {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLFormElement/noValidate)
      */
     noValidate: boolean;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLFormElement/rel) */
     rel: string;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLFormElement/relList) */
     get relList(): DOMTokenList;
     set relList(value: string);
     /**
@@ -11556,7 +11560,7 @@ interface HTMLIFrameElement extends HTMLElement {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLIFrameElement/width)
      */
     width: string;
-    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLIframeElement/getSVGDocument) */
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLIFrameElement/getSVGDocument) */
     getSVGDocument(): Document | null;
     addEventListener<K extends keyof HTMLElementEventMap>(type: K, listener: (this: HTMLIFrameElement, ev: HTMLElementEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
     addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
@@ -11759,6 +11763,7 @@ interface HTMLInputElement extends HTMLElement, PopoverInvokerElement {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLInputElement/defaultValue)
      */
     defaultValue: string;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLInputElement/dirName) */
     dirName: string;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLInputElement/disabled) */
     disabled: boolean;
@@ -14049,6 +14054,7 @@ interface HTMLTextAreaElement extends HTMLElement {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLTextAreaElement/defaultValue)
      */
     defaultValue: string;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLTextAreaElement/dirName) */
     dirName: string;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLTextAreaElement/disabled) */
     disabled: boolean;
@@ -16975,6 +16981,12 @@ interface Navigator extends NavigatorAutomationInformation, NavigatorBadge, Navi
     readonly doNotTrack: string | null;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigator/geolocation) */
     readonly geolocation: Geolocation;
+    /**
+     * Available only in secure contexts.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigator/login)
+     */
+    readonly login: NavigatorLogin;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigator/maxTouchPoints) */
     readonly maxTouchPoints: number;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigator/mediaCapabilities) */
@@ -17133,6 +17145,21 @@ interface NavigatorLocks {
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigator/locks) */
     readonly locks: LockManager;
 }
+
+/**
+ * Available only in secure contexts.
+ *
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/NavigatorLogin)
+ */
+interface NavigatorLogin {
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/NavigatorLogin/setStatus) */
+    setStatus(status: LoginStatus): Promise<void>;
+}
+
+declare var NavigatorLogin: {
+    prototype: NavigatorLogin;
+    new(): NavigatorLogin;
+};
 
 interface NavigatorOnLine {
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigator/onLine) */
@@ -21421,6 +21448,7 @@ declare var SVGGraphicsElement: {
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGImageElement)
  */
 interface SVGImageElement extends SVGGraphicsElement, SVGURIReference {
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGImageElement/crossOrigin) */
     crossOrigin: string | null;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGImageElement/height) */
     readonly height: SVGAnimatedLength;
@@ -21727,8 +21755,11 @@ declare var SVGNumberList: {
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGPathElement)
  */
 interface SVGPathElement extends SVGGeometryElement {
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGPathElement/pathLength) */
     readonly pathLength: SVGAnimatedNumber;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGPathElement/getPointAtLength) */
     getPointAtLength(distance: number): DOMPoint;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGPathElement/getTotalLength) */
     getTotalLength(): number;
     addEventListener<K extends keyof SVGElementEventMap>(type: K, listener: (this: SVGPathElement, ev: SVGElementEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
     addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
@@ -22288,6 +22319,7 @@ interface SVGTextPositioningElement extends SVGTextContentElement {
     readonly dx: SVGAnimatedLengthList;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGTextPositioningElement/dy) */
     readonly dy: SVGAnimatedLengthList;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGTextPositioningElement/rotate) */
     readonly rotate: SVGAnimatedNumberList;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/SVGTextPositioningElement/x) */
     readonly x: SVGAnimatedLengthList;
@@ -27289,6 +27321,8 @@ interface Window extends EventTarget, AnimationFrameProvider, GlobalEventHandler
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/orientation)
      */
     readonly orientation: number;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/originAgentCluster) */
+    readonly originAgentCluster: boolean;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/outerHeight) */
     readonly outerHeight: number;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/outerWidth) */
@@ -28926,6 +28960,8 @@ declare var opener: any;
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/orientation)
  */
 declare var orientation: number;
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/originAgentCluster) */
+declare var originAgentCluster: boolean;
 /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/outerHeight) */
 declare var outerHeight: number;
 /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/outerWidth) */
@@ -29787,6 +29823,7 @@ type KeyUsage = "decrypt" | "deriveBits" | "deriveKey" | "encrypt" | "sign" | "u
 type LatencyMode = "quality" | "realtime";
 type LineAlignSetting = "center" | "end" | "start";
 type LockMode = "exclusive" | "shared";
+type LoginStatus = "logged-in" | "logged-out";
 type MIDIPortConnectionState = "closed" | "open" | "pending";
 type MIDIPortDeviceState = "connected" | "disconnected";
 type MIDIPortType = "input" | "output";
