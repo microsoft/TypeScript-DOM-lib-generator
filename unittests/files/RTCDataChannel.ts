e8df270b1596a6dc9fe8378829a88f3c90c6488d
Bashamega <adambashaahmednaji@gmail.com>
2025-04-25 14:06:32 +0300

    modify tests

--- unittests/files/RTCDataChannel.ts
+++ unittests/files/RTCDataChannel.ts
@@ -1,9 +1,38 @@
-function sendIt (peerConnection: RTCPeerConnection, data: ArrayBufferView | string): void {
-    const dc = peerConnection.createDataChannel('')
-  
-    if (typeof data === 'string') {
-      dc.send(data)
-    } else {
-      dc.send(data)
-    }
-  }
+function sendIt(
+  peerConnection: RTCPeerConnection,
+  data: string | Blob | ArrayBuffer | ArrayBufferView,
+): void {
+    const dc = peerConnection.createDataChannel('');
+    dc.onopen = () => {
+        dc.send(data);
+    };
+
+    dc.onerror = (e) => {
+        console.error("DataChannel error:", e);
+    };
+}
+
+
+const peerConnection = new RTCPeerConnection();
+
+peerConnection.ondatachannel = (event) => {
+    const channel = event.channel;
+    channel.onmessage = (e) => {
+        console.log("Received message:", e.data);
+    };
+};
+
+// Call `sendIt` with a string
+sendIt(peerConnection, "Hello world!");
+
+// Call `sendIt` with a Blob
+const blob = new Blob(["Blob message!"], { type: "text/plain" });
+sendIt(peerConnection, blob);
+
+// Call `sendIt` with an ArrayBuffer
+const arrayBuffer = new TextEncoder().encode("ArrayBuffer message!").buffer;
+sendIt(peerConnection, arrayBuffer);
+
+// Call `sendIt` with an ArrayBufferView
+const view = new Uint8Array([72, 105, 33]); // "Hi!"
+sendIt(peerConnection, view);
