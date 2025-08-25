734e4768eba8ed60372c8f31978b18b2f6c3c14a
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-08-24 21:47:35 -0500

    Add SecurityPolicyViolationEvent & securitypolicyviolation to WorkerGlobal

--- baselines/sharedworker.generated.d.ts
+++ baselines/sharedworker.generated.d.ts
@@ -10318,6 +10318,7 @@ interface WorkerGlobalScopeEventMap {
     "offline": Event;
     "online": Event;
     "rejectionhandled": PromiseRejectionEvent;
+    "securitypolicyviolation": SecurityPolicyViolationEvent;
     "unhandledrejection": PromiseRejectionEvent;
 }
 
@@ -10349,6 +10350,8 @@ interface WorkerGlobalScope extends EventTarget, FontFaceSource, WindowOrWorkerG
     ononline: ((this: WorkerGlobalScope, ev: Event) => any) | null;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/WorkerGlobalScope/rejectionhandled_event) */
     onrejectionhandled: ((this: WorkerGlobalScope, ev: PromiseRejectionEvent) => any) | null;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/WorkerGlobalScope/securitypolicyviolation_event) */
+    onsecuritypolicyviolation: ((this: WorkerGlobalScope, ev: SecurityPolicyViolationEvent) => any) | null;
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/WorkerGlobalScope/unhandledrejection_event) */
     onunhandledrejection: ((this: WorkerGlobalScope, ev: PromiseRejectionEvent) => any) | null;
     /**
@@ -11233,6 +11236,8 @@ declare var onoffline: ((this: SharedWorkerGlobalScope, ev: Event) => any) | nul
 declare var ononline: ((this: SharedWorkerGlobalScope, ev: Event) => any) | null;
 /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/WorkerGlobalScope/rejectionhandled_event) */
 declare var onrejectionhandled: ((this: SharedWorkerGlobalScope, ev: PromiseRejectionEvent) => any) | null;
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/WorkerGlobalScope/securitypolicyviolation_event) */
+declare var onsecuritypolicyviolation: ((this: WorkerGlobalScope, ev: SecurityPolicyViolationEvent) => any) | null;
 /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/WorkerGlobalScope/unhandledrejection_event) */
 declare var onunhandledrejection: ((this: SharedWorkerGlobalScope, ev: PromiseRejectionEvent) => any) | null;
 /**
