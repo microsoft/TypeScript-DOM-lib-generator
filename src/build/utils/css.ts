b7057370d5d737e068d3370d795a68525e3b8a79
Bashamega <adambashaahmednaji@gmail.com>
2025-11-01 15:36:26 +0200

    -

--- src/build/utils/css.ts
+++ src/build/utils/css.ts
@@ -9,14 +9,3 @@ export function camelToHyphenCase(name: string): string {
   const dashPrefix = name.startsWith("webkit") ? "-" : "";
   return dashPrefix + name.replace(/[A-Z]/g, (c) => "-" + c.toLowerCase());
 }
-
-export function generateWebIdlFromCssProperties(properties: string[]): string {
-  return `partial interface CSSStyleProperties {${properties
-    .map(
-      (property) =>
-        `\n  [CEReactions] attribute [LegacyNullToEmptyString] CSSOMString ${hyphenToCamelCase(
-          property,
-        )};`,
-    )
-    .join("")}\n};`;
-}
---
+++ src/build/webref/css.ts
@@ -0,0 +1,11 @@
+import { hyphenToCamelCase } from "../utils/css.js";
+export function generateWebIdlFromCssProperties(properties: string[]): string {
+  return `partial interface CSSStyleProperties {${properties
+    .map(
+      (property) =>
+        `\n  [CEReactions] attribute [LegacyNullToEmptyString] CSSOMString ${hyphenToCamelCase(
+          property,
+        )};`,
+    )
+    .join("")}\n};`;
+}
