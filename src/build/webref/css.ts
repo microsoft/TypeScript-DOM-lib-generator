24f2964f0773678a8f7e99289b8a1635a31588ac
Bashamega <adambashaahmednaji@gmail.com>
2025-11-01 16:33:11 +0200

    -

--- src/build/webref/css.ts
+++ src/build/webref/css.ts
@@ -1,4 +1,15 @@
-import { hyphenToCamelCase } from "../utils/css.js";
+export function hyphenToCamelCase(name: string): string {
+  const camel = name
+    .replace(/^-(\w)/, (_, c) => c)
+    .replace(/-(\w)/g, (_, c) => c.toUpperCase());
+  return camel === "float" ? "_float" : camel;
+}
+
+export function camelToHyphenCase(name: string): string {
+  const dashPrefix = name.startsWith("webkit") ? "-" : "";
+  return dashPrefix + name.replace(/[A-Z]/g, (c) => "-" + c.toLowerCase());
+}
+
 export function generateWebIdlFromCssProperties(properties: string[]): string {
   return `partial interface CSSStyleProperties {${properties
     .map(
