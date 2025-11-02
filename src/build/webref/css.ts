e357f85b56f44af34de1a56f3714786deddac973
Adam Naji <110662505+Bashamega@users.noreply.github.com>
2025-11-02 12:19:00 +0200

    Remove unused camelToHyphenCase function
    Removed the camelToHyphenCase function from css.ts.

--- src/build/webref/css.ts
+++ src/build/webref/css.ts
@@ -1,8 +1,4 @@
 import { hyphenToCamelCase } from "../utils/css.js";
-export function camelToHyphenCase(name: string): string {
-  const dashPrefix = name.startsWith("webkit") ? "-" : "";
-  return dashPrefix + name.replace(/[A-Z]/g, (c) => "-" + c.toLowerCase());
-}
 
 export function generateWebIdlFromCssProperties(properties: string[]): string {
   return `partial interface CSSStyleProperties {${properties
