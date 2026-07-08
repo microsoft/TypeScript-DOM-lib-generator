d0237c71925676769161654426ccce2ce7d82ffa
Souza Gustavo (BD/SWD-FSA1) <sgz2ca@bosch.com>
2026-07-08 11:34:44 -0300

    add type to RequestInit method

--- baselines/ts5.9/serviceworker.generated.d.ts
+++ baselines/ts5.9/serviceworker.generated.d.ts
@@ -970,8 +970,8 @@ interface RequestInit {
     integrity?: string;
     /** A boolean to set request's keepalive. */
     keepalive?: boolean;
-    /** A string to set request's method. */
-    method?: string;
+    /** A string to set request's method. Defaults to "GET". */
+    method?: HTTPMethod;
     /** A string to indicate whether the request will use CORS, or will be restricted to same-origin URLs. Sets request's mode. */
     mode?: RequestMode;
     priority?: RequestPriority;
@@ -13562,6 +13562,7 @@ type GPUSize64 = number;
 type GPUSize64Out = number;
 type GPUStencilValue = number;
 type GPUTextureUsageFlags = number;
+type HTTPMethod = (UppercaseHTTPMethod | Lowercase<UppercaseHTTPMethod>) & {};
 type HashAlgorithmIdentifier = AlgorithmIdentifier;
 type HeadersInit = [string, string][] | Record<string, string> | Headers;
 type IDBValidKey = number | string | Date | BufferSource | IDBValidKey[];
@@ -13584,6 +13585,7 @@ type TimerHandler = string | Function;
 type Transferable = OffscreenCanvas | ImageBitmap | MessagePort | ReadableStream | WritableStream | TransformStream | ArrayBuffer;
 type URLPatternInput = string | URLPatternInit;
 type Uint32List = Uint32Array<ArrayBufferLike> | GLuint[];
+type UppercaseHTTPMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH" | "QUERY";
 type XMLHttpRequestBodyInit = Blob | BufferSource | FormData | URLSearchParams | string;
 type BinaryType = "arraybuffer" | "blob";
 type CSSMathOperator = "clamp" | "invert" | "max" | "min" | "negate" | "product" | "sum";
--- baselines/webworker.generated.d.ts
+++ baselines/webworker.generated.d.ts
@@ -1124,8 +1124,8 @@ interface RequestInit {
     integrity?: string;
     /** A boolean to set request's keepalive. */
     keepalive?: boolean;
-    /** A string to set request's method. */
-    method?: string;
+    /** A string to set request's method. Defaults to "GET". */
+    method?: HTTPMethod;
     /** A string to indicate whether the request will use CORS, or will be restricted to same-origin URLs. Sets request's mode. */
     mode?: RequestMode;
     priority?: RequestPriority;
@@ -15379,6 +15379,7 @@ type GPUSize64 = number;
 type GPUSize64Out = number;
 type GPUStencilValue = number;
 type GPUTextureUsageFlags = number;
+type HTTPMethod = (UppercaseHTTPMethod | Lowercase<UppercaseHTTPMethod>) & {};
 type HashAlgorithmIdentifier = AlgorithmIdentifier;
 type HeadersInit = [string, string][] | Record<string, string> | Headers;
 type IDBValidKey = number | string | Date | BufferSource | IDBValidKey[];
@@ -15402,6 +15403,7 @@ type TimerHandler = string | Function;
 type Transferable = OffscreenCanvas | ImageBitmap | MessagePort | MediaSourceHandle | ReadableStream | WritableStream | TransformStream | AudioData | VideoFrame | RTCDataChannel | ArrayBuffer;
 type URLPatternInput = string | URLPatternInit;
 type Uint32List = Uint32Array<ArrayBufferLike> | GLuint[];
+type UppercaseHTTPMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH" | "QUERY";
 type XMLHttpRequestBodyInit = Blob | BufferSource | FormData | URLSearchParams | string;
 type AacBitstreamFormat = "aac" | "adts";
 type AlphaOption = "discard" | "keep";
